5421f162de9ede9fd549894198aed4e5764da039
Niikogda <niikogda@gmail.com>
2023-08-28 20:18:22 +0300

    save

--- lib/core/internal/app/application.dart
+++ lib/core/internal/app/application.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_test/core/presentation/screens/detail_screen/presentation/detail_screen.dart';
+import 'package:flutter_application_test/core/presentation/screens/home_screen/presentation/home_screen.dart';
+import 'package:flutter_application_test/core/presentation/screens/intro_screen/presentation/intro_screen.dart';
 import 'package:get/get.dart';
 
 class Application extends StatelessWidget {
@@ -6,6 +9,12 @@ class Application extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      initialRoute: '/intro',
+      getPages: [
+      GetPage(name: '/intro', page: () => const IntroScreen()),
+      GetPage(name: '/home', page: () => const HomeScreen()),
+      GetPage(name: '/detail', page: () => const DetailScreen()),
+    ],);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_test/core/internal/app/application.dart';
 
 void main() {
-  runApp();
+  runApp(const Application());
 }
 
